0e4ae77db7e2983ddb7dbe4bbd94859f1529d7ad
bak3839 <bak3839@naver.com>
2026-01-26 16:49:05 +0900

    ✅ test: Redis 직렬화 테스트 제거

--- src/main/java/com/team8/damo/repository/RestaurantRepository.java
+++ src/main/java/com/team8/damo/repository/RestaurantRepository.java
@@ -4,9 +4,8 @@ import com.team8.damo.entity.Restaurant;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface RestaurantMongoRepository extends MongoRepository<Restaurant, String> {
+public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
 
     List<Restaurant> findByCategoryGroupName(String categoryGroupName);
 }
--- src/test/java/com/team8/damo/repository/RestaurantRepositoryTest.java
+++ src/test/java/com/team8/damo/repository/RestaurantRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.team8.damo.repository;
 
 import com.team8.damo.entity.Restaurant;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,20 +13,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
 @ActiveProfiles("test")
-class RestaurantMongoRepositoryTest {
+class RestaurantRepositoryTest {
 
     @Autowired
-    private RestaurantMongoRepository restaurantMongoRepository;
+    private RestaurantRepository restaurantRepository;
 
     @Test
     @DisplayName("식별자로 식당을 조회할 수 있다.")
     void findAll() {
-//        Optional<RestaurantMongo> restaurant = restaurantMongoRepository.findById("6976b57f10e1fa815903d4cf");
-//        System.out.println(restaurant);
-//        assertThat(restaurant).isPresent();
-
-        List<Restaurant> restaurants = restaurantMongoRepository.findByCategoryGroupName("음식점");
-        restaurants.forEach(System.out::println);
+        List<Restaurant> restaurants = restaurantRepository.findByCategoryGroupName("음식점");
         assertThat(restaurants).isNotEmpty();
     }
 }
