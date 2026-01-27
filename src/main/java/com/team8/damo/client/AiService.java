274b77af7f2488adf1dd79ca6e87ecb804187bc7
bak3839 <bak3839@naver.com>
2026-01-27 20:07:32 +0900

    ♻️ refactor: AI 장소 추천 요청 DTO에 그룹 위치(x, y) 추가로 인한 생성 로직 변경

--- src/main/java/com/team8/damo/client/AiService.java
+++ src/main/java/com/team8/damo/client/AiService.java
@@ -4,6 +4,7 @@ import com.team8.damo.client.request.AiRecommendationRequest;
 import com.team8.damo.client.request.DiningData;
 import com.team8.damo.client.response.AiRecommendationResponse;
 import com.team8.damo.entity.Dining;
+import com.team8.damo.entity.Group;
 import com.team8.damo.entity.RecommendRestaurant;
 import com.team8.damo.repository.RecommendRestaurantRepository;
 import com.team8.damo.util.Snowflake;
@@ -23,8 +24,8 @@ public class AiService {
     private final RecommendRestaurantRepository recommendRestaurantRepository;
 
     @Transactional
-    public void recommendationRestaurant(Long groupId, Dining dining, List<Long> userIds) {
-        DiningData diningData = new DiningData(dining.getId(), groupId, dining.getDiningDate(), dining.getBudget());
+    public void recommendationRestaurant(Group group, Dining dining, List<Long> userIds) {
+        DiningData diningData = new DiningData(dining.getId(), group.getId(), dining.getDiningDate(), dining.getBudget(), String.valueOf(group.getLongitude()), String.valueOf(group.getLatitude()));
         AiRecommendationRequest request = new AiRecommendationRequest(diningData, userIds);
         AiRecommendationResponse recommendation = aiClient.recommendation(request);
         recommendation.recommendedItems().forEach(
