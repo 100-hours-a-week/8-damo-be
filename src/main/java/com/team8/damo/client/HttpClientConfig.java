e730ccd55c99802d50b114d8c69b9e7c4196e821
bak3839 <bak3839@naver.com>
2026-02-02 21:56:28 +0900

    ♻️ refactor: RestClient에 읽기 타임아웃 설정 추가 및 Resilience 설정

--- src/main/java/com/team8/damo/client/HttpClientConfig.java
+++ src/main/java/com/team8/damo/client/HttpClientConfig.java
@@ -2,17 +2,24 @@ package com.team8.damo.client;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class HttpClientConfig {
 
     @Bean
     public HttpServiceProxyFactory httpServiceProxyFactory() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setReadTimeout(Duration.ofSeconds(150));
+
         RestClient restClient = RestClient.builder()
             .baseUrl("https://damo.today")
+            .requestFactory(requestFactory)
             .build();
 
         return HttpServiceProxyFactory.builder()
---
+++ src/main/java/com/team8/damo/config/ResilienceConfig.java
@@ -0,0 +1,9 @@
+package com.team8.damo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.resilience.annotation.EnableResilientMethods;
+
+@Configuration
+@EnableResilientMethods
+public class ResilienceConfig {
+}
