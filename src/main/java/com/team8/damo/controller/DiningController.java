79b0ef846f4fbfa28b72f1fa05dfa698da7f9606
bak3839 <bak3839@naver.com>
2026-02-28 15:34:06 +0900

    ♻️ refactor: 추론 내역 조회를 커서 기반에서 전체 조회로 변경

--- src/main/java/com/team8/damo/controller/DiningController.java
+++ src/main/java/com/team8/damo/controller/DiningController.java
@@ -175,10 +175,8 @@ public class DiningController implements DiningControllerDocs {
     public BaseResponse<CursorPageResponse<RecommendationStreamingResponse>> getRecommendationStreaming(
         @PathVariable Long groupId,
         @PathVariable Long diningId,
-        @RequestParam(required = false) Long cursor,
-        @RequestParam(required = false) int limit,
         @AuthenticationPrincipal JwtUserDetails user
     ) {
-        return BaseResponse.ok(diningService.getRecommendationStreaming(diningId, cursor, limit));
+        return BaseResponse.ok(diningService.getRecommendationStreaming(diningId));
     }
 }
