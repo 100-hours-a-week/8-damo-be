9054cfd12921425b4a8e6d3ef6db82a20d61293f
bak3839 <bak3839@naver.com>
2026-03-09 10:42:36 +0900

    ✨ feat: Redisson 기반 분산 락 구현

---
+++ src/main/java/com/team8/damo/lock/local/RedisLock.java
@@ -0,0 +1,46 @@
+package com.team8.damo.lock.local;
+
+import com.team8.damo.aop.CustomLock;
+import com.team8.damo.lock.LockStrategy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
+@Slf4j
+@Primary
+@Component
+@RequiredArgsConstructor
+public class RedisLock implements LockStrategy {
+
+    private final RedissonClient redissonClient;
+
+    @Override
+    public Object execute(String key, CustomLock customLock, ProceedingJoinPoint joinPoint) throws Throwable {
+        RLock rLock = redissonClient.getLock(key);
+
+        try {
+            boolean available = rLock.tryLock(customLock.waitTime(), customLock.leaseTime(), customLock.timeUnit());
+            if (!available) {
+                return false;
+            }
+
+            return joinPoint.proceed();
+        } catch (InterruptedException e) {
+            throw new InterruptedException();
+        }  finally {
+            try {
+                rLock.unlock();
+            } catch (IllegalMonitorStateException e) {
+                log.info("Redisson Lock Already UnLock {} ",
+                    kv("key", key)
+                );
+            }
+        }
+    }
+}
